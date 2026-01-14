d3fee350e9945872cc55d386fa9f29ce2370f70e
melvyn920 <129944544+melvyn920@users.noreply.github.com>
2026-01-14 13:34:37 +0000

    update dockerfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "Building Docker image..."
-docker build -t my-blog-api:latest -t my-blog-api:$GITHUB_SHA ./dockerfile
+docker build -t my-blog-api:latest -t my-blog-api:$GITHUB_SHA ./docker
 docker tag my-blog-api:latest my-blog-api:${GITHUB_SHA}
